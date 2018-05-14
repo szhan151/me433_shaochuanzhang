6f9052aa42c57d7ff083e92757dcc4dfd049d94a
szhan151 <zhangshaochuan73@gmail.com>
2018-05-14 11:32:07 -0500

    hw11

--- hw11/firmware/hw11.X/build/default/production/_ext/1360937237/app.o.d
+++ hw11/firmware/hw11.X/build/default/production/_ext/1360937237/app.o.d
@@ -196,4 +196,7 @@ build/default/production/_ext/1360937237/app.o:  \
  ../../../../framework/usb/src/usb_device_function_driver.h  \
  ../../../../framework/usb/usb_hid.h  \
 ../src/app.h  \
-../src/mouse.h 
+../src/mouse.h  \
+ ../src/ST7735.h  \
+../src/i2c_master_noint.h  \
+../src/imc.h 
